a9047c8ec52b35270eaab227bd4e6f8ce7b28d4b
Grant Goodman <grantgoodman@optonline.net>
2015-10-18 14:59:32 -0400

    Initial Commit

---
+++ Key Club/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Key Club
+//
+//  Created by Grant Goodman on 10/18/15.
+//  Copyright © 2015 NeoTechnica Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Key Club/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Key Club
+//
+//  Created by Grant Goodman on 10/18/15.
+//  Copyright © 2015 NeoTechnica Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
